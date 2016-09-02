d851683af85af6da4e972d46a778b8e459291d08
Robert Yuen <dungeonry@hotmail.com>
2016-09-01 23:51:18 -0700

    Turns out I was missing the CSS for the other d3 graph before.  Appended dataset to vis tab.

--- frontendchallenge/2016-aug-18/visualization.js
+++ frontendchallenge/2016-aug-18/visualization.js
@@ -1,22 +1,37 @@
-d3.select("#chart")
-  .selectAll("div")
-  .data([4,8,15,16,23,42])
-  .enter()
-  .append("div")
-  .style("height", function(d){
-    return d + "px";
-  });
+$(document).ready(function(){
+  $.get("./dataset.csv", function(data){
+    var formattedData = $.csv.toObjects(data);
+    var remunerationData = [];
+    for(var i =0; i < formattedData.length; i++){
+      remunerationData.push(parseFloat(formattedData[i].Remuneration.replace(/,/g , ""))/10000);
+    }
 
-var colorMap = d3.interpolateRgb(
-  d3.rgb("#d6e685"),
-  d3.rgb("#1e6823")
-);
+    d3.select("#chart")
+      .selectAll("div")
+      .data(remunerationData)
+      .enter()
+      .append("div")
+      .style("height", function(d){
+        return d + "px";
+      });
 
-d3.select("#heatmap")
-  .selectAll("div")
-  .data([.2, .4, 0, 0, .13, .92])
-  .enter()
-  .append("div")
-  .style("background-color", function(d){
-    return (d === 0) ? "#eee": colorMap(d);
+    var colorMap = d3.interpolateRgb(
+      d3.rgb("#d6e685"),
+      d3.rgb("#1e6823")
+    );
+
+    var heatremuneration = remunerationData.map(function(currItem){
+      return currItem/10;
+    });
+
+    d3.select("#heatmap")
+      .selectAll("div")
+      .data(heatremuneration)
+      .enter()
+      .append("div")
+      .style("background-color", function(d){
+        return (d === 0) ? "#eee": colorMap(d);
+      });
+
   });
+});
