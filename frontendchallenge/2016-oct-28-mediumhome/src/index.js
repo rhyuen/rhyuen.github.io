9aa0aab787b3fe4daa612f54c2e28dcda199e9ef
Robert Yuen <dungeonry@hotmail.com>
2016-11-01 20:51:48 -0700

    Feed User Details more or less done.  Styled the Signup Ad.

--- frontendchallenge/2016-oct-28-mediumhome/src/index.js
+++ frontendchallenge/2016-oct-28-mediumhome/src/index.js
@@ -1,1 +1,5 @@
-console.log("I'm here.");
+if(/Android|iPhone|iPad|iPod|BlackBerry|Opera Mini/i.test(navigator.userAgent)){
+  console.log("Mobile Browser in use. Redirect to mobile site.");
+}else{
+  console.log("Desktop Browser in use.");
+}
--- js/index.js
+++ js/index.js
@@ -7,3 +7,10 @@ if("serviceWorker" in navigator){
       console.log("Service Worker Registered");
     });
 }
+
+
+if(/Android|iPhone|iPad|iPod|BlackBerry|Opera Mini/i.test(navigator.userAgent)){
+  console.log("Mobile Browser in use.");
+}else{
+  console.log("Desktop Browser in use.");
+}
