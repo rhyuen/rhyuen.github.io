c18227a69eb67e109678365d283e721a05ee9873
Robert Yuen <dungeonry@hotmail.com>
2016-11-08 17:31:58 -0800

    Removed dummy static posts.  Added add new link entry UI and new link generator.

--- frontendchallenge/2016-nov-5-instapaper/src/index.js
+++ frontendchallenge/2016-nov-5-instapaper/src/index.js
@@ -1,5 +1,38 @@
-console.log("hi, js here.");
+console.log("[%s] Instapaper Clone", new Date().toLocaleString());
 
 $(document).ready(function(){
 
+  var count = 0;
+  var postLimit = 12;
+  while(count < postLimit){
+      $("#main_frame").append(makePost());
+      count++;
+  }
+
+
+
+  function makePost(){
+    return(
+      $("<div/>", {class: "post"})
+        .append($("<div/>", {class: "post_title"})
+        .append($("<div/>", {text: "Post Title"})
+        .append($("<span/>", {class:"post_edit"})
+        .append($("<a/>", {href: "#", text: "Edit"})
+      ))))
+        .append($("<div/>", {class: "post_content", text: "Post Content"}))
+        .append($("<div/>", {class: "post_bottom"})
+          .append($("<div/>", {class: "post_meta", text: "Relative Time * N min Read"}))
+          .append($("<div/>", {class: "post_social", text: "Icons Go here."}))
+        )
+    );
+  }
 });
+
+$("#addlink").click(function(){  
+  $("#signup_page").css({width: "100%", display: "block"});
+});
+
+// $("*").not($("#addlink")).click(function(){
+//   if($("#signup_page").width() === "100%"))
+//     $("$signup_page").css({width: "0%"});
+// });
